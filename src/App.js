1f33950d2c64f0885be050d0b9006593399cef0e
A B <ab218@live.com>
2019-10-06 23:35:36 -0700

    QOL improvements. Copy to clipboard automatically, auto focus inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
+function copyToClipboard(text) {
+  const dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
+
 function PreGameState({digits, setDigits, setMode, generateRandomNumber}) {
-  function startGame() {
-    setMode('PLAY');
-    generateRandomNumber(digits);
+  function startGame(e) {
+    if (!e.keyCode || e.keyCode === 13) {
+      setMode('PLAY');
+      generateRandomNumber(digits);
+    }
   }
   function setCorrectDigits(value) {
     if (!value || value <= 0) {
@@ -18,7 +29,7 @@ function PreGameState({digits, setDigits, setMode, generateRandomNumber}) {
   }
   return <span>
     <h5>{`Set Length (current: ${digits})`}</h5>
-    <input onChange={e=>setCorrectDigits(parseInt(e.target.value))} />
+    <input autoFocus onKeyDown={e => startGame(e)} onChange={e=>setCorrectDigits(parseInt(e.target.value))} />
     {/* Add option to enter number (not random) */}
     <button onClick={startGame}>Start Game</button>
   </span>
@@ -32,22 +43,29 @@ function WinState({restartGame}) {
 }
 
 function GameState({answer, setNumberInput, numberInput, checkCombo, counter}) {
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return <>
     <h1>{answer}</h1>
     <h5>Choose a number:</h5>
-    <input onChange={e => setNumberInput(e.target.value)} style={{width: 300, alignSelf: 'center'}} value={numberInput} />
+    <input ref={inputRef} autoFocus onKeyDown={e => checkCombo(e)} onChange={e => setNumberInput(e.target.value)} style={{width: 300, alignSelf: 'center'}} value={numberInput} />
     <button style={{alignSelf: 'center', marginTop: 10, height: 20, width: 200}} onClick={checkCombo}>Check Combination</button>
     {(counter !== null) && `${counter} clicks.`}
   </>
 }
 
 function Guesses({guesses}) {
-  return <>
+  return <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
     <h1>Guesses:</h1>
-    <ol style={{display: 'flex', flexWrap: 'wrap'}}>
-      {guesses.length > 0 && guesses.map((g, i) =><li key={i} style={{padding: 30, marginRight: 20}}>{`[${g.guess}] - ${g.clicks} clicks`}</li>)}
+    <ol style={{width: 200}}>
+      {guesses.length > 0 && guesses.map((g, i) =><li key={i} style={{marginTop: 10}}>{`[${g.guess}] - ${g.clicks} clicks`}</li>)}
     </ol>
-  </>
+  </div>
 }
 
 function App() {
@@ -69,8 +87,10 @@ function App() {
     setAnswer(Math.floor(Math.random()*(10 ** (length - 1) * 9)) + 10**(length -1));
   }
 
-  function checkCombo() {
+  function checkCombo(e) {
     if (!answer || !numberInput) return;
+    // keyCode 13 === Enter
+    if (e.keyCode && e.keyCode !== 13) return;
     const splitRandomNumber = answer.toString().split('');
     const splitNumberInput = numberInput.split('');
     let counter = 0;
@@ -80,6 +100,8 @@ function App() {
       }
     }
     const guessObj = {guess: numberInput, clicks: counter}
+    // For each guess, the result is copied to clipboard
+    copyToClipboard(`[${numberInput}] - ${counter} clicks.`);
     setGuesses(old => old.concat(guessObj));
     setCounter(counter);
     setNumberInput('');
